7861e55c8307eb0c0d857013d883643645147b38
Ruilin Xu <xurlin7@mail2.sysu.edu.cn>
2025-02-27 01:28:18 +0800

    title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -35,7 +35,7 @@ class HomeComponent extends React.Component<WithTranslation> {
 
                             </div>
                             <div className='text-xl md:text-5xl mb-2 block sm:hidden'>
-                                Anonymous Chat Room
+                                Nebulink
                             </div>
                         </div>
                     ) : (
